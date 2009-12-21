2ddfc4d1c3f1782b52b00d91d5e62711cd5a0dd1
Christian Bolstad <kers@atdt.nu>
2009-12-21 18:30:27 +0100

    GPL notice in

--- twitterlink.php
+++ twitterlink.php
@@ -6,6 +6,20 @@ Description: Create a href-link to twitter @usernames mentioned in posts.
 Version: The 1.03
 Author: Christian Bolstad
 Author URI: http://christianbolstad.se
+
+ This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 */
 
 function cb_twitterlink($content)
